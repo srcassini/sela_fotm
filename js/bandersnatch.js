f81b8e74762f42ac8cffc5894607128dc6666e82
Shaun R Cassini <srcassini1@sheffield.ac.uk>
2020-08-18 18:43:29 +0100

    done

--- js/bandersnatch.js
+++ js/bandersnatch.js
@@ -1,76 +1,89 @@
-// 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
-
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady() {
 player = new YT.Player('player', {
     height: '390',
     width: '640',
-    videoId: 'Ch5MEJk5ZCQ',
+    videoId: 'Ch5MEJk5ZCQ', //intro video id
     events: {
     'onReady': onPlayerReady,
     'onStateChange': onPlayerStateChange
     }
 });
 }
-
-// 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
-event.target.playVideo();
+    event.target.playVideo();
 }
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
 var done = false;
-function onPlayerStateChange(event) {
+function onPlayerStateChange(event){
 if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 3000);
     // load scenario 1
-    player.loadVideoById('B6Lh-TB2_mA',0);
     player.playVideo();
     done = true;
 }
 }
-function stopVideo() {
-    player.stopVideo();
-}
 
-// current intro video: [next intro video, option 1 video, option 2 video]
+// current option 2 video: merged (next intro video, option 1 video, option 2 video)
 var videoPaths = {
-    'B6Lh-TB2_mA': ['1YTeasbvJ2E','st8-EY71K84','mbnBYh-BJ1g'],
-    'ADrBo7u3tR4': ['EdzQ9wEOElw', 'UR_byRbXxvs', 'XeFxdkaFzRA']
+    'Ch5MEJk5ZCQ': 'mbnBYh-BJ1g', // intro -> disasters
+    'mbnBYh-BJ1g': 'XeFxdkaFzRA', // disasters -> antibiotics
+    'XeFxdkaFzRA': 'jHs5POy8-8Y', // antibiotics -> food
+    'jHs5POy8-8Y': 'jJA3dYwIB3s', // food -> water
+    'jJA3dYwIB3s': '110iUX1Ursk' // water -> renewables
 };
 
-var list = [];
-var currentId = '';
-var loadUrl = '';
+var conclusions = [
+    ['XP8BUWr4AT4', '6Jz0JcQYtqo'],
+    ['Z-BbpaNXbxg', 'J4gJY_5eNqQ'],
+    ['XVWQCd5j_ec', 'o5Ejup_nfPY'],
+    ['5DH9IZ01Qqg', 'wFAR3WggSRk'],
+    ['CNrF0JbDVc8', 'Q1zbgd6xpGQ']
+]
 
-
-function option1() {
-    currentId = getVideoId(player.getVideoUrl());
-    targetId = videoPaths[currentId];
-    list.push(1);
-}
-
-function option2() {
-    player.loadVideoById("1YTeasbvJ2E",0);
-    player.playVideo();
-    list.push(2);
-}
+var decisions = [];
+var endPlaylist = [];
+var currentId = 'Ch5MEJk5ZCQ';
 
 function getListState() {
-    console.log(list);
+    console.log(decisions);
 }
 
 function getVideoId(url) {
     id = url.substring(url.indexOf('=')+1, url.length);
     return id;
 }
+function playSequence(id) {
+    player.loadVideoById(id);
+    player.playVideo();
+}
 
+function option1() {
+    decisions.push(0);
+    currentId = getVideoId(player.getVideoUrl());
+    targetId = videoPaths[currentId];
+    playSequence(targetId);
+}
+
+function option2() {
+    decisions.push(1);    
+    currentId = getVideoId(player.getVideoUrl());
+    targetId = videoPaths[currentId];
+    playSequence(targetId);
+}
+
+function confirmChoices() {
+    for (i=0; i<decisions.length; i++){
+        if (decisions[i] == 0) {
+            endPlaylist.unshift(conclusions[i][0]);
+        } else {
+            endPlaylist.unshift(conclusions[i][1]);
+        };
+    };
+    player.loadPlaylist(endPlaylist);
+    player.playVideo();
+}
